7459bdeab9e97929ee72af6263aafcec4f4efba9
Giovanni <5774411+giovannicoppola@users.noreply.github.com>
2022-09-21 07:46:49 -0400

    bibtex

---
+++ source/build_bibtex.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3 
+#
+# using the BibTeX background export
+
+#
+#New York – Partly cloudy ⛅️  🌡️+66°F (feels +66°F, 68%) 🌬️↘4mph 🌘 Wed Sep 21 06:23:37 2022
+#W38Q3 – 264 ➡️ 100 – 191 ❇️ 173
+
+import bibtexparser # did not work, error with non-quoted entries (eg month)
+import json
+import sqlite3
+BIBTEX_FILE = "/Users/giovanni/Library/CloudStorage/GoogleDrive-giovannicoppola@gmail.com/My Drive/Tsu_ASO-RNAi.bib"
+INDEX_DB = "indexTEST.db"
+
+
+def JSONtoDB (myJSON,myTable, mydatabase):
+    column_list = []
+    column = []
+    
+        
+    for data in myJSON:
+        
+        column = list(data.keys())
+        for col in column:
+            if col not in column_list:
+                column_list.append(col)
+
+    value = []
+    values = [] 
+    for data in myJSON:
+        for i in column_list:
+            value.append(str(dict(data).get(i)))   
+        values.append(list(value)) 
+        value.clear()
+        
+    
+    
+    create_statement = "create VIRTUAL table " + myTable + " USING FTS3 ({0})".format(" text,".join(column_list))
+      
+    insert_statement = "insert into " + myTable + " ({0}) values (?{1})".format(",".join(column_list), ",?" * (len(column_list)-1))    
+    drop_statement = "DROP TABLE IF EXISTS "+ myTable  
+
+    # execution	
+    db=sqlite3.connect(mydatabase)
+    c = db.cursor()   
+    c.execute(drop_statement)
+    c.execute(create_statement)
+    c.executemany(insert_statement , values)
+    values.clear()
+    db.commit()
+
+
+
+with open(BIBTEX_FILE) as bibtex_file:
+    #bib_database = bibtexparser.load(bibtex_file)
+    bib_database = bibtexparser.bparser.BibTexParser(common_strings=True).parse_file(bibtex_file) # this is for the string in the month
+
+#print(bib_database.entries)
+print (type(bib_database.entries))
+myJSONlist = (bib_database.entries)
+print (type(myJSONlist))
+for myyy in myJSONlist:
+    print (type (myyy))
+    print (list (myyy.keys()))
+myJSON = json.dumps(bib_database.entries)
+
+print (type(myJSON))
+JSONtoDB (myJSONlist, "myTTable", INDEX_DB)
+
+print (myJSON)
+
+
+
+
+
+
+
+
